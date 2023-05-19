8369fede5227be73ffc3cc1fc31471d7ba7cefbb
Web HM <133254421+webhm@users.noreply.github.com>
2023-05-19 16:58:45 -0500

    dev

--- index.php
+++ index.php
@@ -1,23 +1,6 @@
 <?php
 
-$dsn="(DESCRIPTION=( ADDRESS_LIST= (ADDRESS= (PROTOCOL=TCP) (HOST=172.16.3.247) (PORT=1521)))( CONNECT_DATA= (SID=conclina) ))";
-$user="mchang";
-$password="1501508480";
-  
- //storing connection id in $conn
-$conn=odbc_connect($dsn,$user, $password);
-
-//Checking connection id or reference
-if (!$conn)
- {
- echo (die(odbc_error()));
- }
- else
-{
-    echo "Connection Successful !";
-}
-//Resource releasing
-odbc_close($conn);
+phpinfo();
 
 
 ?>
