e5acbe69b36e3ae6311581d23d815bd1d81011b5
martinxapps <52510496+martinxapps@users.noreply.github.com>
2023-05-21 19:54:50 -0500

    dev1

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php
 // Las cosas
 
-print_r(
-    PDO::getAvailableDrivers()
-);
+print_r(PDO::getAvailableDrivers());
+
+
+$driver = "odbc";
+$host = "172.16.3.247";
+$user = "mchang";
+$pass = '1501508480';
+$dbName = "conclina";
+$charset = "utf8";
+
+
+
+$pdo = newPDO("{$this->driver}:host={$this->host};port=1521;dbname={$this->dbName};charset={$this->charset}", $this->user, $this->pass);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+return $pdo;
+die($e->getMessage());
+
+
 
 ?>
