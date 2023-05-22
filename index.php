274c956c1649e8f5ae1d84f06cb54d4166ff215d
martinxapps <52510496+martinxapps@users.noreply.github.com>
2023-05-21 19:16:14 -0500

    dev1

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
+$tns = "
+(DESCRIPTION =
+    (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 172.16.3.247)(PORT = 1521)) )
+    (CONNECT_DATA = (SERVICE_NAME = conclina) )
+)";
 
-phpinfo();
+//user :: Ex -> hr
+//pass:: Ex ->  12345
 
+try {
+    $conn = new PDO("oci:dbname=" . $tns, 'mchang', '1501508480');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'connect Successfull';
+} catch (PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
 
 ?>
