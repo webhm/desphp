87657aa6cd336ac9a8c9a0c3ac67632047d1f4bc
martinxapps <52510496+martinxapps@users.noreply.github.com>
2023-05-21 19:21:04 -0500

    dev.con

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
-$tns = "
-(DESCRIPTION =
-    (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 172.16.3.247)(PORT = 1521)) )
-    (CONNECT_DATA = (SERVICE_NAME = conclina) )
-)";
 
-//user :: Ex -> hr
-//pass:: Ex ->  12345
-
-try {
-    $conn = new PDO("odbc:dbname=" . $tns, 'mchang', '1501508480');
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo 'connect Successfull';
-} catch (PDOException $e) {
-    echo 'ERROR: ' . $e->getMessage();
-}
+print_r(PDO::getAvailableDrivers());
 
 ?>
